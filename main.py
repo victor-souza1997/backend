a44eda14aefe0906e8eea575b46d073903400f5e
victor-souza1997 <victor-souza1997@hotmail.com>
2022-07-22 17:47:31 -0300

    feat: adicao de algumas checagens de informacao

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import os
+import json
+from flask import Response
+from flask import jsonify
+from bson.json_util import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ['MONGO_URI']
@@ -8,11 +12,16 @@ app.config["MONGO_URI"] = os.environ['MONGO_URI']
 mongo = PyMongo(app)
 
 
-@app.route('/input_data/<sensor_id>/<value>')
-def save_data_sensor(sensor_id, value):
+@app.route('/input_data/<device_id>/<value>')
+def save_data_device(device_id, value):
     try:
-        mongo.db.data.insert_one(
-            {'sensor': sensor_id, 'value': value})
+        devices = [device for device in mongo.db.devices.find(
+            {'device_id': device_id})]
+        if devices:
+            mongo.db.data.insert_one(
+                {'device_id': device_id, 'value': value})
+        else:
+            return "device not in the system"
     except Exception as e:
         return 'not saved'
     return "saved with sucess"
@@ -20,32 +29,37 @@ def save_data_sensor(sensor_id, value):
 
 @app.route('/get_devices')
 def get_devices():
-    try:
-        devices_list = mongo.db.devices.find()
-    except:
-        return 'not found'
-    return str(devices_list)
+    # try:
+    devices_list = mongo.db.devices.find()
+    devices_list = [device for device in devices_list]
+    print(devices_list)
+    # print(json.dumps(devices_list))
+    if devices_list:
+        return Response(json.dumps(devices_list, default=str), mimetype="application/json")
+
+    else:
+        return None
 
 
-@app.route('/set_newdevice/<sensor_id>')
-def set_device():
+@app.route('/set_newdevice/<device_id>')
+def set_device(device_id):
     try:
+        devices = [device for device in mongo.db.devices.find(
+            {'device_id': device_id})]
+        if devices:
+            return "already up"
         mongo.db.devices.insert_one(
-            {'device': 'device_id',
-             'name': 'device_name',
-             'type': 'device_type',
-             'status': 'device_status'})
-
+            {'device_id': device_id})
     except:
         return 'not saved'
     return "saved with sucess"
 
 
-@ app.route('/get_data/<sensor_id>')
-def get_data(sensor_id):
-    data_list = mongo.db.data.find({'sensor': sensor_id})
+@ app.route('/get_data/<device_id>')
+def get_data(device_id):
+    data_list = mongo.db.data.find({'device_id': device_id})
     datas = [data for data in data_list]
-    return str(datas)
+    return Response(json.dumps(datas, default=str), mimetype="application/json")
 
 
 if __name__ == '__main__':

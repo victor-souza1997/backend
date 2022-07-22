dc9530d8d73012079b704a2a69f7f30004d6234e
victor-souza1997 <victor-souza1997@hotmail.com>
2022-07-22 13:23:55 -0300

    feat: adiciona função de adição de dispositivo ao banco

--- main.py
+++ main.py
@@ -9,22 +9,39 @@ mongo = PyMongo(app)
 
 
 @app.route('/input_data/<sensor_id>/<value>')
-def save_data(sensor_id, value):
-    print(sensor_id, value)
-
-    mongo.db.data.insert_one(
-        {'sensor': sensor_id, 'value': value})
-
-    return 200
+def save_data_sensor(sensor_id, value):
+    try:
+        mongo.db.data.insert_one(
+            {'sensor': sensor_id, 'value': value})
+    except Exception as e:
+        return 'not saved'
+    return "saved with sucess"
 
 
 @app.route('/get_devices')
 def get_devices():
-    devices_list = mongo.db.devices.find()
-    return devices_list
+    try:
+        devices_list = mongo.db.devices.find()
+    except:
+        return 'not found'
+    return str(devices_list)
 
 
-@app.route('/get_data/<sensor_id>')
+@app.route('/set_newdevice/<sensor_id>')
+def set_device():
+    try:
+        mongo.db.devices.insert_one(
+            {'device': 'device_id',
+             'name': 'device_name',
+             'type': 'device_type',
+             'status': 'device_status'})
+
+    except:
+        return 'not saved'
+    return "saved with sucess"
+
+
+@ app.route('/get_data/<sensor_id>')
 def get_data(sensor_id):
     data_list = mongo.db.data.find({'sensor': sensor_id})
     datas = [data for data in data_list]
